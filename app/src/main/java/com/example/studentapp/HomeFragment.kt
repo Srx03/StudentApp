27834525aa22fa03df4a997d58fad1133a0cfc78
Djordje Djuric <djordjedjuric05@gmail.com>
2022-10-15 01:34:21 +0200

    commit

--- app/src/main/java/com/example/studentapp/HomeFragment.kt
+++ app/src/main/java/com/example/studentapp/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.studentapp.databinding.FragmentHomeBinding
 
@@ -32,6 +33,7 @@ class HomeFragment : Fragment() {
 
         binding.btnAddSubject.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_subjectFragment)
+            Toast.makeText(context, "Yes", Toast.LENGTH_SHORT).show()
         }
 
 
